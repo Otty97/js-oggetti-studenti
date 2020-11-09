3e378015bf1f1502719779b33c069f37a72b9ee0
Othman Taik <taikothman1997@gmail.com>
2020-11-09 16:23:25 +0100

    js script working
    need HTML integration

--- js/main.js
+++ js/main.js
@@ -2,49 +2,49 @@ $(document).ready(function() {
 
   // Creo un oggetto che descrive uno studente con le seguenti proprietà: nome, cognome e età.
   var studente = {
-    "nome": " ",
-    "cognome": " ",
-    "eta": 0
+    "nome": "A",
+    "cognome": "B",
+    "eta": 0,
   }
 
   // Stampo tutte le proprietà dell'oggetto studente
-for (var key in studente) {
-  console.log(studente[key]);
+  for (var key in studente) {
+   console.log(studente[key]);
 }
 
   // Creo un array di oggetti di studenti.
   var studenti = [{
       "nome": "Pinco",
       "cognome": "Pallino",
-      "eta": 28
+      "eta": 28,
     },
     {
       "nome": "Tizio",
       "cognome": "Caio",
-      "eta": 26
+      "eta": 26,
     },
     {
       "nome": "Asdrubale",
       "cognome": "Asdrubalini",
-      "eta": 23
+      "eta": 23,
     },
     {
       "nome": "Tal",
       "cognome": "Dei Tali",
-      "eta": 31
+      "eta": 31,
     },
     {
       "nome": "Othman",
       "cognome": "Taik",
-      "eta": 23
+      "eta": 23,
     }
   ]
 
-  $('#listaStudenti').text(studente)
+
 
   // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
   $('button').click(function(){
-  var nuovoStudente = studente;
+  var nuovoStudente = {};
   nuovoStudente.nome = prompt("inserisci il tuo nome");
   nuovoStudente.cognome = prompt("inserisci il tuo cognome");
   nuovoStudente.eta = parseInt(prompt("inserisci la tua età"));
@@ -54,7 +54,7 @@ for (var key in studente) {
 
   // Ciclo su tutti gli studenti e stampo per ognuno nome e cognome
   for (var i = 0; i < studenti.length; i++) {
-    var studenteLista = studenti[i];
-    $('#listaStudenti').text(studenti)
+    var studentiLista = studenti[i];
+    console.log(studentiLista);
   }
 });
